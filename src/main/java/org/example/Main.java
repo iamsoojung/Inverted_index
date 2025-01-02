3c35ca519ea98888de1d417e53c9bcf52e5acc8d
iamsoojung <crystar2402@naver.com>
2025-01-02 17:03:03 +0900

    feat: 입력 텍스트 전처리 메서드 구현 (String class 활용)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,10 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(processText("apple a a what???").toString());
+        System.out.println(processTextByPattern("apple a a what???").toString());
     }
+
+
 
     /**
      * 입력된 텍스트를 전처리하여 단어 추출
@@ -17,13 +19,35 @@ public class Main {
      * @param text 입력된 원본 텍스트
      * @return 전처리된 단어 리스트
      */
-    public static List<String> processText(String text) {
+    public static List<String> processTextByPattern(String text) {
+        if (text == null || text.isEmpty()) {
+            return new ArrayList<>();
+        }
+
         Pattern pattern = Pattern.compile("\\b[a-zA-Z]+\\b");   // 정규식 패턴 생성
         Matcher matcher = pattern.matcher(text.toLowerCase());  // 소문자 변환 후 정규식 매칭
 
         List<String> words = new ArrayList<>();
         while (matcher.find()) {    // 정규식에 일치하는 단어 찾기
             words.add(matcher.group()); // 찾은 단어 추가
+        }
+        return words;
+    }
+
+    /**
+     * Pattern 클래스 대신 String 클래스로 전처리
+     */
+    public static List<String> processText(String text) {
+        if (text == null || text.isEmpty()) {
+            return new ArrayList<>();
+        }
+        String[] splitWords = text.toLowerCase().split("[^a-zA-Z]+");
+
+        List<String> words = new ArrayList<>();
+        for (String word : splitWords) {
+            if (!words.isEmpty()) {
+                words.add(word);
+            }
         }
         return words;
     }
