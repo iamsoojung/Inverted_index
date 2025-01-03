ff5e59fc30bcfd080d99b72eba956b835f7cd3d7
iamsoojung <crystar2402@naver.com>
2025-01-03 10:38:16 +0900

    feat: 역색인 생성 메서드 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,18 +18,22 @@ public class Main {
         String inputFile = args[1];
         String outputFile = args[2];
 
-        // To do : 로직 처리 필요
+        // 입력 파일을 라인 단위로 읽기
         List<String> lines = readInputFile(inputFile);
-        for (String line : lines) {
-            System.out.println(processTextByPattern(line));
-        }
+
+        // 역색인 생성
+        Map<String, Map<Integer, Integer>> invertedIndex = createInvertedIndex(lines);
+        System.out.println(invertedIndex.toString());
+
+        // 출력 정렬
+
+        // 출력 파일 쓰기
 
     }
 
 
     /**
      * 입력 파일 읽어서, 라인 단위로 반환함
-     *
      * @param filePath
      * @return
      * @throws IOException
@@ -36,6 +42,30 @@ public class Main {
         return Files.readAllLines(Paths.get(filePath));
     }
 
+    /**
+     * 라인 별로 문서아이디와 텍스트 분리 후, 역색인 생성
+     * @param lines
+     * @return
+     */
+    private static Map<String, Map<Integer, Integer>> createInvertedIndex (List<String> lines) {
+        Map<String, Map<Integer, Integer>> invertedIndex = new HashMap<>();     // 역색인 맵
+
+        for (String line : lines) {
+            String[] parts = line.split(" ", 2);    //공백 기준으로 ID, 텍스트 분리
+            if (parts.length != 2)  continue;   // 포맷 이상하면 continue
+
+            int docId = Integer.parseInt(parts[0]);     // 문서 ID
+            List<String> words = processTextByPattern(parts[1]);    // 텍스트 (단어 추출하여 분리)
+
+            for (String word : words) {
+                Map<Integer, Integer> docMap = invertedIndex.getOrDefault(word, new HashMap<>());   // 역색인에 없는 단어면 새 맵 추가
+                docMap.put(docId, docMap.getOrDefault(docId, 0) + 1);   // 문서 ID의 빈도 업데이트
+                invertedIndex.put(word, docMap);    // 역색인에 저장
+            }
+        }
+
+        return invertedIndex;
+    }
 
     /**
      * 입력된 텍스트를 전처리하여 단어 추출
@@ -43,7 +73,7 @@ public class Main {
      * @param text 입력된 원본 텍스트
      * @return 전처리된 단어 리스트
      */
-    public static List<String> processTextByPattern(String text) {
+    private static List<String> processTextByPattern(String text) {
         if (text == null || text.isEmpty()) {
             return new ArrayList<>();
         }
@@ -61,7 +91,7 @@ public class Main {
     /**
      * Pattern 클래스 대신 String 클래스로 전처리
      */
-    public static List<String> processText(String text) {
+    private static List<String> processText(String text) {
         if (text == null || text.isEmpty()) {
             return new ArrayList<>();
         }
---
+++ src/main/java/input.xs
@@ -0,0 +1,3 @@
+1 It is what it is
+2 What? What is is is it
+3 It is a banana. It!
