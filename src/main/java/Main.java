8d2e9aa569908ba4bf40e5471a86fffa162ebefe
iamsoojung <crystar2402@naver.com>
2025-01-06 16:15:51 +0900

    refactor: directory 구조 변경(i/o, bin 추가)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -15,15 +16,26 @@ public class Main {
                 System.out.println("땡~! java Main index <inputFile> <outputFile>");
                 return;
             }
-            String inputFile = "./input/" + args[1];
-            String outputFile = "./output/" + args[2];
+            String inputFile = "../input/" + args[1];
+            String outputDir = "../output/";
+            String outputFile = outputDir + args[2];
+
+            File dir = new File(outputDir);     // output 디렉토리 없으면 만들도록
+            if (!dir.exists()) {
+                if (dir.mkdirs()) {
+                    System.out.println("output 디렉토리가 생성되었습니다: " + outputDir);
+                } else {
+                    System.out.println("output 디렉토리를 생성할 수 없습니다. 권한을 확인하세요.");
+                    return;
+                }
+            }
             InvertedIndex.runIndexing(inputFile, outputFile);
         } else if ("search".equalsIgnoreCase(mode)) {
             if (args.length < 3) {
                 System.out.println("땡~! java Main search <indexFile> <query>");
                 return;
             }
-            String indexFile = "./output/" + args[1];
+            String indexFile = "../output/" + args[1];
             String query = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
             SearchEngine.runSearch(indexFile, query);
         } else {
